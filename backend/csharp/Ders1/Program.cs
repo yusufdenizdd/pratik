6c629e576d99de8ed75ae59071e949e2c1fb2375
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2025-09-26 11:23:29 +0300

    c#

--- backend/csharp/Ders1/Program.cs
+++ backend/csharp/Ders1/Program.cs
@@ -45,8 +45,49 @@ namespace Ders1
             foreach (string kurs in kurslar)
             {
                 Console.WriteLine(kurs);
-                
+
+            }
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "c#";
+            kurs1.Egitmen = "engin demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "java";
+            kurs2.Egitmen = "kerem";
+            kurs2.IzlenmeOrani = 0;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "python";
+            kurs3.Egitmen = "berkay";
+            kurs3.IzlenmeOrani = 20;
+
+            Console.WriteLine(kurs1.KursAdi + ": " + kurs1.Egitmen);
+
+            Kurs[] kurslar2 = new Kurs[] {
+                kurs1, kurs2, kurs3
+            };
+
+            foreach (Kurs kurs in kurslar2)
+            {
+                Console.WriteLine(kurs.KursAdi);
             }
         }
+
+
+
     }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public string Egitmen { get; set; }
+
+        public int IzlenmeOrani { get; set; }
+    }
+
+
+
+
 }
--- backend/csharp/Ders1/obj/Debug/net9.0/Ders1.AssemblyInfo.cs
+++ backend/csharp/Ders1/obj/Debug/net9.0/Ders1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ders1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a8c26edc354d28b429d1b66c85b7a7f9bd3c5dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fada3efa0ecd794c68821ce8db9046577dd9d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ders1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ders1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
