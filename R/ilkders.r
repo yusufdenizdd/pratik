217012206bb4572d0d59ca63c39c239f6fe70387
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2025-10-08 13:02:18 +0300

    R

--- R/ilkders.r
+++ R/ilkders.r
@@ -42,6 +42,7 @@ a <- "veri madenciliği"
 as.integer()
 as.numeric()
 
+# hata verecektir
 durum <- TRUE
 as.numeric(durum)
 
@@ -50,11 +51,27 @@ durum <- as.numeric(durum)
 
 is.numeric(durum)
 
+# is.integer
 x <- as.integer(100)
 is.integer(x)
+
+# değişkenin türünü doğrudan öğrenmek istiyorsak typeof()
+
 typeof(x)
 x <- as.character(x)
 typeof(x)
+
+# Değişken türünü doğrudan öğrenmek istiyorsak typof()
+x <- 125
+as.character(x)
+# hafızada tumadık: aş. şekilde dene
+x <- as.character(x)
+# veritipini test edek
+typeof(x)
+
+typof(sonuc)
+
+
 
 # mantıksalişlemler
 a <- 125
@@ -73,6 +90,11 @@ show(iris)
 # view(adult)
 iris
 # tüm transactionlar gözüküyor
+
+# en üstteki satırları görüntüle
+head(iris)
+head(iris, 10)
+
 
 # 2.hazır veri kümeleri
 data()
@@ -105,3 +127,45 @@ str(iris)
 # mevcut çalışma alanımızda ne var: ls()
 # çalışma dizinini görüntüle getwd()
 # çalışma dizinini değiştir setwd('dosyayolu')
+
+# 3. R Paketler
+# syntax install.packages
+install.packages("arules")
+
+install.packages("caret")
+
+
+# ara yüzden Tools->Install Packages
+
+
+# yüklüolan kütüphanelerinin görüntülenmesi
+search()
+
+
+# paketlerin oturuma tanıtılması
+# kütüphanelerin yüklenmesi
+library(arules)
+
+
+
+
+# 4. Çalışma Alanı
+# Mevcut çalışma alanımızda neler var
+ls()
+
+
+# çalışma diziini göürntüle
+getwd()
+
+
+# çalışma dizinini değiştir
+# setwd('C:\Users\gkara\Dozuments\R\www')
+
+
+# sorular
+# 1- bir paket yüklendikten sonra ne olur?
+# 2- x=45 6. değişkeni yapıp bu değişkenşerin tümünü integara dönüştür
+# 3-Bir paket yüklendikten sonra R oturumuna nasıl tanıtılır
+# 4-R da mevcut çalışma alanını görüntüle
+# 5-a b c isimli nesneleri tanımşla
+# 6- R çalışma alanı C bla workspace
