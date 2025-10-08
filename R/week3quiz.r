cb48bf43123ee467b604349c197df787d26a2c26
Yusuf Deniz DOĞAN <114568545+yusufdenizdd@users.noreply.github.com>
2025-10-08 17:03:45 +0300

    .

---
+++ R/week3quiz.r
@@ -0,0 +1,31 @@
+### WEEK 3- QUIZ #####
+
+##SORULAR##
+
+#1.R’nin iris isimli veri kümesinin ilk altı satırını ve ilk 3 satırını görüntüleyiniz. Bu veri kümesi hangi değişkenlere veya hangi sütunlara sahiptir ?
+
+#2.R’ın BOD isimli veri kümesinin tüm satırlarını görüntüleyiniz. Bu veri kümesi hangi değişkenlere sahiptir. İpucu: data()-attributes
+
+#3.R’nın iris isimli veri kümesi kaç adet satır ve sütundan oluşmaktadır. R fonksiyonları yardımı ile listeleyiniz.
+
+#4.BOD isimli veri kümesi hakkında özet bilgiyi görüntüleyiniz.
+
+#5.cluster isimli R paketini yükleyiniz.
+
+#6.R konsolu üzerinden e1071 isimli paketin fonksiyonları ile ilgili listeyi içeren yardım sayfalarına nasıl ulaşılabilir?
+
+#7. R'da bir paket yüklendikten sonra R oturumuna nasıl tanıtılır ? 
+
+#8.R’da mevcut çalışma alanınını görüntüleyiniz.
+
+#9.R’ın çalışma alanı «C://belgeler/R/workspace» olarak belirleyiniz.
+
+#10.a, b, c isimli nesneleri tanımlayınız. (değer veriniz)
+
+#11.Mevcut yaratılmış nesneleri görüntüleyiniz
+
+#12.c isimli nesneyi siliniz.
+
+#13.Bu nesnelerin türünü görüntüleyiniz.
+
+#14.X=45.6 değişkeni yaratıp, bu değişkenin türünü integer'a dönüştürünüz.
